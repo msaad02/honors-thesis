dd3e229d8e1b0e35d1df9720f5234f8780f4e14b
Matthew Saad <msaad02@outlook.com>
2023-09-10 16:22:28 -0400

    class for categorized search engine

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,9 @@
-from chatgpt_pe.test import test
+from chatgpt_pe.categorized_engine import QuestionAnswering
+import os
 
-t = test()
 
-print(t.add())
+qa_bot = QuestionAnswering()
+
+answer, price = qa_bot("How can I apply to SUNY Brockport?")
+
+print(answer)
