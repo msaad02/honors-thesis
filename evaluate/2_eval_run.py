b9bc24542b23c3e7f14284c2701504dba3ce9787
Matthew Saad <msaad02@outlook.com>
2024-04-03 10:50:00 -0400

    rag eval complete?

--- evaluate/2_eval_run.py
+++ evaluate/2_eval_run.py
@@ -18,22 +18,22 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fine_tuning.finetune_class import FineTunedModel
 from scratch_model.inference import ScratchModel
 from text_search.rag import RAG
-from datasets import load_dataset
 from termcolor import colored
 from tqdm import tqdm
+import pandas as pd
 
 # ----- Loading in models/data -----
 models = {
     "RAG": RAG(
-        main_categorization_model_dir="../text_search/models/main_category_model",
+        main_categorization_model_dir="../text_search/models/main_category",
         subcategorization_model_dir="../text_search/models/subcategory_models/",
         embeddings_file="../text_search/data/embeddings.pickle"),
     "Finetuned": FineTunedModel(repo_id="msaad02/BrockportGPT-7b"),
     "Scratch": ScratchModel(model_dir="../scratch_model/models/transformer_v7/")
 }
 
-data = load_dataset("msaad02/brockport-gpt-4-qa")['test'].to_pandas()
-data = data.loc[:, ["question", "answer"]]
+data = pd.read_csv('./data/evaluation_data.csv').sample(n=2)
+# data = data.loc[:, ["question", "answer"]]
 questions = data["question"].to_list()
 
 
@@ -60,13 +60,13 @@ for name, model in models.items():
     if name == "RAG":
         args['config'] = {
             "search_type": "typesense",
-            "use_classifier": True,
-            "n_results": 3,
-            "model_kwargs": {"temperature": 0.8},
+            "use_classifier": False,
+            "n_results": 5,
+            "model_kwargs": {"temperature": 0.0},
         }
 
     data[name] = eval_model(name, model, **args)
 
 
 # ----- Saving the results -----
-data.to_csv("./data/test_answers.csv", index=False)
+data.to_csv("./data/evaluation_output.csv", index=False)
